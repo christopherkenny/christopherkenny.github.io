b1d5095a11a95024566d012f6d35094ee33367a6
Christopher Kenny <ctk44@cornell.edu>
2023-12-30 00:08:46 -0500

    update icons

--- docs/site_libs/quarto-nav/quarto-nav.js
+++ docs/site_libs/quarto-nav/quarto-nav.js
@@ -85,17 +85,6 @@ window.document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  function dashboardOffset() {
-    const dashboardNavEl = window.document.getElementById(
-      "quarto-dashboard-header"
-    );
-    if (dashboardNavEl !== null) {
-      return dashboardNavEl.clientHeight;
-    } else {
-      return 0;
-    }
-  }
-
   function updateDocumentOffsetWithoutAnimation() {
     updateDocumentOffset(false);
   }
@@ -103,7 +92,7 @@ window.document.addEventListener("DOMContentLoaded", function () {
   function updateDocumentOffset(animated) {
     // set body offset
     const topOffset = headerOffset();
-    const bodyOffset = topOffset + footerOffset() + dashboardOffset();
+    const bodyOffset = topOffset + footerOffset();
     const bodyEl = window.document.body;
     bodyEl.setAttribute("data-bs-offset", topOffset);
     bodyEl.style.paddingTop = topOffset + "px";
@@ -124,11 +113,9 @@ window.document.addEventListener("DOMContentLoaded", function () {
       if (window.Headroom && sidebar.classList.contains("sidebar-unpinned")) {
         sidebar.style.top = "0";
         sidebar.style.maxHeight = "100vh";
-        sidebar.style.minHeight = "100vh";
       } else {
         sidebar.style.top = topOffset + "px";
         sidebar.style.maxHeight = "calc(100vh - " + topOffset + "px)";
-        sidebar.style.minHeight = "calc(100vh - " + topOffset + "px)";
       }
     });
 
