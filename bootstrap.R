ce683489e5fd67b3b5e94a38b8bee3bb7d64625b
Christopher Kenny <ctk44@cornell.edu>
2024-06-24 23:16:42 -0400

    translate to htmltags

--- bootstrap.R
+++ bootstrap.R
@@ -1,61 +1,98 @@
-# built to follow https://getbootstrap.com/docs/5.1/components/carousel/
-make_carousel <- function(id, items, alt) {
+library(htmltools)
+
+make_carousel <- function(id, items, alt, class_opts = '', ...) {
   
   if (missing(alt) || length(alt) < length(items)) {
     alt <- paste0('Slide ', seq_along(items))
   }
-  
-  out <- paste0('<div id="', id, '" class="carousel slide" data-bs-ride="carousel">')
-  
-  out <- c(out, make_carousel_indicators(id, items))
 
-  out <- c(out, make_carousel_items(id, items, alt))
-
-  out <- c(out, 
-    paste0('  <button class="carousel-control-prev" type="button" data-bs-target="#', id, '" data-bs-slide="prev">'),
-    '    <span class="carousel-control-prev-icon" aria-hidden="true"></span>',
-    '    <span class="visually-hidden">Previous</span>',
-    '  </button>',
-    paste0('  <button class="carousel-control-next" type="button" data-bs-target="#', id, '" data-bs-slide="next">'),
-    '    <span class="carousel-control-next-icon" aria-hidden="true"></span>',
-    '    <span class="visually-hidden">Next</span>',
-    '  </button>',
-    '</div>'
+  indicators <- make_carousel_indicators(id, items)
+  
+  items <- make_carousel_items(id, items, alt)
+  
+  prev_button <- tags$button(
+    class = 'carousel-control-prev',
+    type = 'button',
+    `data-bs-target` = paste0('#', id),
+    `data-bs-slide` = 'prev',
+    tags$span(class = 'carousel-control-prev-icon', `aria-hidden` = TRUE),
+    tags$span(class = 'visually-hidden', 'Previous')
   )
   
-  cat(out, sep = '\n')
+  next_button <- tags$button(
+    class = 'carousel-control-next',
+    type = 'button',
+    `data-bs-target` = paste0('#', id),
+    `data-bs-slide` = 'next',
+    tags$span(class = 'carousel-control-next-icon', `aria-hidden` = TRUE),
+    tags$span(class = 'visually-hidden', 'Next')
+  )
+  
+  div(
+    id = id, 
+    class = paste('carousel slide', class_opts), 
+    ...,
+    `data-bs-ride` = 'carousel',
+    indicators,
+    items,
+    prev_button,
+    next_button
+  )
 }
 
-
-make_carousel_items <- function(id, items, alt) {
-  out <- '  <div class="carousel-inner">'
-  
-  for (i in seq_along(items)) {
-    if (i == 1) {
-      out <- c(out, paste0('    <div class="carousel-item active">'))
-    } else {
-      out <- c(out, '    <div class="carousel-item">')
-    }
-    out <- c(out, paste0('      <img src="', items[i], '" class="d-block w-100" alt="', alt[i], '">'))
-    out <- c(out, '    </div>')
-  }
-  
-  out <- c(out, '  </div>')
-  out
-}
 
 make_carousel_indicators <- function(id, items) {
-  out <- '  <div class="carousel-indicators">'
-  
-  for (i in seq_along(items)) {
+  btns <- lapply(seq_along(items), function(i) {
     if (i == 1) {
-      out <- c(out, paste0('    <button type="button" data-bs-target="#', id, '" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>'))
+      tags$button(
+        type = 'button',
+        `data-bs-target` = paste0('#', id),
+        `data-bs-slide-to` = 0,
+        class = 'active',
+        `aria-current` = TRUE,
+        `aria-label` = 'Slide 1'
+      )
     } else {
-      out <- c(out, paste0('    <button type="button" data-bs-target="#', id, '" data-bs-slide-to="', i - 1L, '" aria-label="Slide ', i,'"></button>'))
+      tags$button(
+        type = 'button',
+        `data-bs-target` = paste0('#', id),
+        `data-bs-slide-to` = i - 1L,
+        `aria-label` = paste0('Slide ', i)
+      )
     }
-  }
+  })
   
-  out <- c(out, '  </div>')
+  div(
+    class = 'carousel-indicators',
+    tagList(btns)
+  )
+}
+
+make_carousel_items <- function(id, items, alt) {
+  items <- lapply(seq_along(items), function(i) {
+    if (i == 1) {
+      tags$div(
+        class = 'carousel-item active',
+        tags$img(
+          src = items[i],
+          class = 'd-block w-100',
+          alt = alt[i]
+        )
+      )
+    } else {
+      tags$div(
+        class = 'carousel-item',
+        tags$img(
+          src = items[i],
+          class = 'd-block w-100',
+          alt = alt[i]
+        )
+      )
+    }
+  })
   
-  out
+  div(
+    class = 'carousel',
+    tagList(items)
+  )
 }
