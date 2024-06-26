d68d7c47c973f549faa29913cf392df5049f9627
Christopher Kenny <ctk44@cornell.edu>
2024-06-25 20:22:15 -0400

    handle dois

--- webscripts/pkgs_csv_to_yml.R
+++ webscripts/pkgs_csv_to_yml.R
@@ -1,6 +1,27 @@
 library(dplyr)
 library(readr)
 
+if (FALSE) {
+  pkgs <- read_csv('pkgs.csv')
+  
+  descs <- lapply(pkgs$name, \(x) desc::desc(package = x))
+  descs <- sapply(descs, \(x) x$get_field('Description'))
+  pkgs$description <- descs
+
+  pkgs$description <- pkgs$description |> 
+    sapply(
+      function(x) {
+        x |> 
+          markdown::markdownToHTML(fragment.only = TRUE) |> 
+          stringr::str_remove_all(pattern = '<p>|</p>|\n')
+      }) |> 
+    stringr::str_replace_all(pattern = '‘', replacement = "'") |> 
+    stringr::str_replace_all(pattern = '’', replacement = "'")
+  
+  pkgs |> 
+    write_csv('pkgs.csv')
+}
+
 pkgs <- read_csv('pkgs.csv') |> 
   mutate(
     logo = paste0('hexes/hex_', name, '.png'),
